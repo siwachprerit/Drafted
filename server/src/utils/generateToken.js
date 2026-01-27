4321559fa085549127a3ed9b5e6c23efc1787cd6
siwachprerit <preritsiwach1.com>
2026-01-27 15:42:05 +0530

    fixed jwt error

--- server/src/utils/generateToken.js
+++ server/src/utils/generateToken.js
@@ -3,7 +3,7 @@ import jwt from 'jsonwebtoken';
 
 const generateToken = (userId) => {
     return jwt.sign({ id: userId }, process.env.JWT_SECRET, {
-        expiresIn: process.env.JWT_EXPIRES_IN,
+        expiresIn: process.env.JWT_EXPIRES_IN || '7d',
     });
 };
 
